cb4d793e6a28a9755cecb954ae5d01662ccd201a
Francesc Via <f@gaxan.com>
2014-07-09 17:53:36 +0100

    removed race condition on tests

--- test_message.py
+++ test_message.py
@@ -1,26 +1,27 @@
 
 import unittest
-import time_utils
+from datetime import datetime
 
 from message import Message
-
-from datetime import datetime, timedelta
-
-
+from time_utils import ut_time
 
 
 class testMessage( unittest.TestCase ):
   def test_Message_now(self):
+    
+    initial_time =  datetime.now()
+    ut_time.fastenNow( initial_time ) 
     
     msg = Message("Hi Boy!" )
     self.assertEqual( msg.timedText(), "Hi Boy! (0 seconds ago)"  )
 
   def test_Message_10_minutes_later(self):
     
-    time_utils.ut_time.fastenNow( datetime.now())
+    initial_time =  datetime.now()
+    ut_time.fastenNow( initial_time)
     msg = Message("Hi Peter!")
     
-    time_utils.ut_time.fastenNow( time_utils.ut_time.now()+timedelta(0,10*60))
+    ut_time.fastenNow( initial_time, deltaMinutes=10)
     self.assertEqual( msg.timedText(), "Hi Peter! (10 minutes ago)"  )
 		
 
